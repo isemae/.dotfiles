d357fbd8b9cbc60579807713da7b3a1bbf2fe865
isemae <foxlis97@naver.com>
2023-10-13 09:06:04 +0900

    zsh and nvim configs

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -5,3 +5,8 @@
 local map = vim.keymap.set
 
 map("i", "<C-d>", "<right><C-h>", { desc = "", remap = true })
+map("i", "<M-f>", "<C-Right>", { desc = "", remap = true })
+map("n", "<M-f>", "w", { desc = "", remap = true })
+map("i", "<M-b>", "<C-Left>", { desc = "", remap = true })
+map("n", "<M-b>", "b", { desc = "", remap = true })
+map({ "n", "t" }, "<C-z>", "<C-/>", { desc = "", remap = true })
