42d5b33413ea7d2743dc5381d280b2b4ed7cf8a6
isemae <foxlis97@naver.com>
2024-08-14 22:48:57 +0900

    update .wezterm.lua

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -8,50 +8,63 @@ if wezterm.target_triple == "aarch64-apple-darwin" then
 
 	-- font_dirs    = { '$HOME/.dotfiles/.fonts' }
 	config.font = wezterm.font_with_fallback({
-		-- {
-		-- 	family = "JetBrainsMonoNL Nerd Font Mono",
-		-- 	-- family = "IosevkaTerm Nerd Font Mono",
-		-- 	weight = "Regular",
-		-- },
+		{
+			family = "JetBrainsMonoNL Nerd Font Mono",
+			weight = "Medium",
+			-- 	-- family = "IosevkaTerm Nerd Font Mono",
+			-- 	weight = "Regular",
+		},
 		{ family = "D2CodingLigature Nerd Font Mono", weight = "Medium" },
 	})
 
-	config.font_size = 17
+	config.font_size = 16
 	config.line_height = 1.1
-	config.freetype_load_flags = "NO_HINTING"
 	config.front_end = "WebGpu"
 
 	-- config.color_scheme = "terafox"
 	-- config.color_scheme = "Maia (Gogh)"
 	-- config.color_scheme = "Adventure"
 	-- config.color_scheme = "Catppuccin Mocha (Gogh)"
-	config.color_scheme = "rose-pine-moon"
+	-- config.color_scheme = "Rosé Pine Moon (Gogh)"
+
 	config.colors = {
+		foreground = "#e0def4",
+		background = "#232136",
+		-- background = "#1f1f28",
+
+		ansi = { "#393552", "#eb6f92", "#3e8fb0", "#f6c177", "#9ccfd8", "#c4a7e7", "#ea9a97", "#e0def4" },
+		brights = { "#6e6a86", "#eb6f92", "#3e8fb0", "#f6c177", "#9ccfd8", "#c4a7e7", "#ea9a97", "#e0def4" },
+
+		selection_fg = "#e0def4",
+		selection_bg = "#44415a",
+
 		cursor_bg = "#f0d2bd",
 		cursor_fg = "black",
-		cursor_border = "#e0e0c9",
+		cursor_border = "#f0d2bd",
+
+		-- tab styles
 		tab_bar = {
 			active_tab = {
-				bg_color = "#36364a",
+				bg_color = "none",
 				fg_color = "#f0d29d",
 			},
 			inactive_tab = {
-				bg_color = "#1c1c27",
+				bg_color = "none",
 				fg_color = "#aaa",
 			},
 			new_tab = {
-				bg_color = "#101112",
+				bg_color = "none",
 				fg_color = "#aaa",
 			},
-			inactive_tab_edge = "#1e1e20",
+			inactive_tab_edge = "none",
 		},
 	}
 
 	config.window_frame = {
 		font = wezterm.font({ family = "D2CodingLigature Nerd Font Mono Nerd", italic = false, weight = "Bold" }),
 		font_size = 14.0,
-		active_titlebar_bg = "#242431",
-		inactive_titlebar_bg = "#242431",
+		active_titlebar_bg = "none",
+		inactive_titlebar_bg = "none",
 	}
 	config.window_padding = {
 		left = 8,
@@ -69,14 +82,14 @@ if wezterm.target_triple == "aarch64-apple-darwin" then
 	config.window_decorations = "INTEGRATED_BUTTONS | RESIZE"
 	config.enable_tab_bar = true
 	config.tab_bar_at_bottom = true
-	config.show_new_tab_button_in_tab_bar = false
+	config.show_new_tab_button_in_tab_bar = true
 	config.show_tab_index_in_tab_bar = false
 	config.hide_tab_bar_if_only_one_tab = true
 	-- config.show_close_tab_button_in_tabs = false -- nightly only for now
 
 	config.window_close_confirmation = "NeverPrompt"
-	-- config.window_background_opacity = 0.95
-	-- config.macos_window_background_blur = 20
+	config.window_background_opacity = 0.93
+	config.macos_window_background_blur = 60
 
 	config.cursor_blink_rate = 600
 	config.default_cursor_style = "BlinkingBlock"
@@ -133,7 +146,7 @@ if wezterm.target_triple == "x86_64-pc-windows-msvc" then
 	-- font_dirs = {
 	--     'C:\\Users\\whoami\\.dotfiles\\.fonts'
 	-- }
-	default_prog = { "wsl.exe", "~", "-d", "Ubuntu-20.04" }
+	-- default_prog = { "wsl.exe", "~", "-d", "Ubuntu-20.04" }
 end
 
 if wezterm.target_triple == "x86_65-unknown-linux-gnu" then
